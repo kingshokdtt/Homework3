89f5eb44e0a95770a0437bfac97d9536a376b8ea
Kingshok <kingshok.datta@student.csulb.edu>
2022-06-13 22:42:23 -0700

    Homework3

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './screens/Home';
-import ProfileScreen from './screens/Profile';
+import HomeScreen from './screens/Home/index.jsx';
+import ProfileScreen from './screens/Profile/index.jsx';
+
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
-
   return (
+
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Welcome' }}
+          options={{ title: 'Want to be Rich!!!'}}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        
+        <Stack.Screen 
+          name="Profile" 
+          component={ProfileScreen}
+          options={{title:'Done'}} 
+        />     
+        
+
       </Stack.Navigator>
+
     </NavigationContainer>
   );
 }
+
+
--- screens/Home/Styles.js
+++ screens/Home/Styles.js
@@ -4,14 +4,25 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 50,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  
   },
+
+  text: {
+    fontWeight:'bold',
+    fontSize:15,
+  },
+
+
   logo: {
     width: 66,
     height: 58,
   },
+
+
 });
 
 export default styles;
